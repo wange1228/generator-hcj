068c531cc4ffdda677af1baecc5737eb7c09d75f
wange <i@wange.im>
2016-06-20 12:06:40 +0800

    新增非 AMD 规范的模块加载

--- generators/app/templates/demo/mobile/srcScripts/hcj-demo/hcj-demo.js
+++ generators/app/templates/demo/mobile/srcScripts/hcj-demo/hcj-demo.js
@@ -1,12 +1,12 @@
-define('hcj-demo', ['zepto'], function($) {
+define('hcj-demo', ['zepto', 'md5'], function($) {
     function HcjDemo() {
-
         this.init();
     }
 
     HcjDemo.prototype = {
         init: function() {
             console.log($);
+            console.log(hex_md5);
         }
     };
 
