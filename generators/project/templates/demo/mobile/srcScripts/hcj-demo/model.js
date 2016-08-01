a5c97f1b0e029a24e65d1c3823c159f9f92caf0d
guchunjie <milo.gu@vipshop.com>
2016-08-01 15:49:05 +0800

    支持移动端开发

---
+++ generators/project/templates/demo/mobile/srcScripts/hcj-demo/model.js
@@ -0,0 +1,24 @@
+define(function() {
+
+    var K = Backbone.Model.extend({
+        // query1: function(data){
+        //     return $.ajax({
+        //         url: 'XXX',
+        //         type: 'GET',
+        //         dataType: 'json',
+        //         data: data || {}
+        //     });
+        // },
+        // query2: function(data){
+        //     return $.ajax({
+        //         url: 'XXX',
+        //         type: 'GET',
+        //         dataType: 'json',
+        //         data: data || {}
+        //     });
+        // }
+    });
+
+    return K;
+   
+});
