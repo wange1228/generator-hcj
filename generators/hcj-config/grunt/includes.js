c58688070a33b327ec589412c3f4b6d20d4338a0
guchunjie <milo.gu@vipshop.com>
2016-11-22 17:55:47 +0800

    多级目录创建的文件包含其它html

--- generators/hcj-config/grunt/includes.js
+++ generators/hcj-config/grunt/includes.js
@@ -23,29 +23,30 @@ class Includes{
         let project = path.project;
         let includeFProp = page ? project + '/' + page : project;
         let htmlExt = path.env == 'desktop' ? '.ftl' : '.html';
-        let includeFSrc = page ? (page + htmlExt) : ('**/*'+htmlExt);
 
-        let srcPage = path.srcPage;
         //支持多级目录创建的文件包含其它html
         if(page){
+          let includeFSrc = page + htmlExt;
+          let srcPage = path.srcPage;
           let folderSeperatorIndex = page.lastIndexOf('/');
           if(folderSeperatorIndex !== -1){
             srcPage = srcPage + '/' + page.slice(0, folderSeperatorIndex);
             includeFSrc = page.slice(folderSeperatorIndex + 1) + htmlExt;
           }
+          this.task.includes[includeFProp] = {
+              options: {
+                  includePath: srcPage
+              },
+              files: [{
+                  expand: true,
+                  cwd: srcPage,
+                  src: [includeFSrc],
+                  dest: path.distPage,
+                  ext: htmlExt
+              }]
+          };
         }
-        this.task.includes[includeFProp] = {
-            options: {
-                includePath: srcPage
-            },
-            files: [{
-                expand: true,
-                cwd: srcPage,
-                src: [includeFSrc],
-                dest: path.distPage,
-                ext: htmlExt
-            }]
-        };
+
 
     }
 
