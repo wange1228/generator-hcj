75effcbc05efde1d1e94e4ad92d4ac6a93be2bb4
guchunjie <milo.gu@vipshop.com>
2016-11-22 18:13:15 +0800

    多级目录创建的文件包含其它html

--- generators/hcj-config/grunt/includes.js
+++ generators/hcj-config/grunt/includes.js
@@ -23,16 +23,26 @@ class Includes{
         let project = path.project;
         let includeFProp = page ? project + '/' + page : project;
         let htmlExt = path.env == 'desktop' ? '.ftl' : '.html';
+        let includeFSrc = page ? (page + htmlExt) : ('**/*'+htmlExt); //如shanghaibank, activity都只放了目录
 
+        let srcPage = path.srcPage;
+        let needIncludes = true;
         //支持多级目录创建的文件包含其它html
         if(page){
-          let includeFSrc = page + htmlExt;
-          let srcPage = path.srcPage;
           let folderSeperatorIndex = page.lastIndexOf('/');
           if(folderSeperatorIndex !== -1){
             srcPage = srcPage + '/' + page.slice(0, folderSeperatorIndex);
             includeFSrc = page.slice(folderSeperatorIndex + 1) + htmlExt;
           }
+        }else{
+          //如页面名为a/b时整个目录不进行匹配操作
+          util.projects.forEach(function(o, i) {
+            if(o.indexOf(':'+project+':') !== -1 && o.indexOf('/') !== -1){
+              needIncludes = false;
+            }
+          });
+        }
+        if(needIncludes){
           this.task.includes[includeFProp] = {
               options: {
                   includePath: srcPage
